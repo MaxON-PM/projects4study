89e5d8cd8c0dcfc3b36e1acedc0cb83b0a06a1bc
Tatyana <tanyach@aurocraft.com>
2020-09-28 21:01:52 +0300

    JS lessons 1

---
+++ first.js
@@ -0,0 +1,15 @@
+"use strict"
+let massage;
+massage ='Привет мир ';
+alert (massage);
+let admin = 'Джон';
+let name;
+name = admin;
+alert (name);
+let isGreater = 4 > 1;
+alert (isGreater);
+//следующий урок
+let result = prompt ("Как тебя зовут", '');
+alert (`Привет ${result}`);
+let qestion = confirm ("Ты петух?");
+let answer = qestion;
