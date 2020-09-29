51b7efa15013db519ef4c80e732cb6dc6a1e2fd9
Tatyana <tanyach@aurocraft.com>
2020-09-29 09:27:56 +0300

    if else tasks

--- first.js
+++ first.js
@@ -1,15 +1,36 @@
 "use strict"
-let massage;
-massage ='Привет мир ';
-alert (massage);
-let admin = 'Джон';
-let name;
-name = admin;
-alert (name);
-let isGreater = 4 > 1;
-alert (isGreater);
+//let massage;
+//massage ='Привет мир ';
+//alert (massage);
+//let admin = 'Джон';
+//let name;
+//name = admin;
+//alert (name);
+//let isGreater = 4 > 1;
+//alert (isGreater);
 //следующий урок
-let result = prompt ("Как тебя зовут", '');
-alert (`Привет ${result}`);
-let qestion = confirm ("Ты петух?");
-let answer = qestion;
+//let result = prompt ("Как тебя зовут", '');
+//alert (`Привет ${result}`);
+//let qestion = confirm ("Ты петух?");
+//let answer = qestion;
+//day 2
+/*let name = prompt ("What is oficial name of JavaScript?",'');
+    if (name == "ECMAScript") {
+        alert ("It`s right")
+    }else {
+        alert ("you are stupid")
+    }*/
+var number = prompt ("Enter any numbet please", "");
+    if (number > 0){
+        alert ("Плюсовое число то")
+    }
+    else if (number < 0){
+        alert ("Ну так это же сраной минусовое число")
+    }
+    else {
+        alert ("Это ноль, придурок")
+    }
+    let result;
+
+    (a + b < 4) ? result = 'Мало': result = 'Много';
+
